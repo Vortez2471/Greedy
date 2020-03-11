68d9eac1921707d88b0acf485b655825e34a2900
Ritwik Baranwal <45766294+Vortez2471@users.noreply.github.com>
2020-03-11 20:06:38 +0530

    Add files via upload

---
+++ greedy1.cpp
@@ -0,0 +1,35 @@
+//counting money problem
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define ll long long
+
+vector<int>vec;
+int main()
+{
+	int arr[]={1,2,5,10,50,100};
+	int n=sizeof(arr)/sizeof(arr[0]);
+
+    int deno;
+    cin>>deno;
+    while(deno)
+    {
+    	int i=n-1;
+    	while(i>=0)
+    	{
+    		if(deno>=arr[i])
+    		{
+    			deno=deno-arr[i];
+    			vec.push_back(arr[i]);
+    			break;
+    		}
+    		else
+    		{
+    			i--;
+    		}
+    	}
+    }
+    for(int i=0;i<vec.size();i++)
+    	cout<<vec[i]<<" ";
+}
