c754e62b55ba6c7d50850002148eaefca7066b80
Ritwik Baranwal <45766294+Vortez2471@users.noreply.github.com>
2020-03-11 20:07:51 +0530

    Add files via upload

---
+++ greedy6.cpp
@@ -0,0 +1,30 @@
+//Chopsticks codechef
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+#define ll long long
+
+int main()
+{
+	ll n,d;
+	cin>>n>>d;
+	ll arr[n];
+	for(ll i=0;i<n;i++)
+		cin>>arr[i];
+	sort(arr,arr+n);
+	ll flag=0;
+	for(ll i=0;i<n;)
+	{
+		if(arr[i+1]-arr[i]<=d)
+		{
+			flag++;
+			i=i+2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	cout<<flag<<endl;
+}
